9e41484852445770556df49820f3c3b8654cdece
EMMANUEL OYEKAN <emmanueloyekan33@gmail.com>
2022-07-04 13:42:28 +0100

    end of project module 4

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import Layout from "../components/layout.js/layout";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
